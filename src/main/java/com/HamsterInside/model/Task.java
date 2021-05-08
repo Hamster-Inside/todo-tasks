9ed25bb23621d94b88741f204c226a9e020a6546
Crypto-Hamster <arkadiusz.budkowski.dev@gmail.com>
2021-05-08 16:43:49 +0200

    2021-05-08-poczatek-lekcji-12

---
+++ src/main/java/com/HamsterInside/model/Task.java
@@ -0,0 +1,37 @@
+package com.HamsterInside.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private boolean isDone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
