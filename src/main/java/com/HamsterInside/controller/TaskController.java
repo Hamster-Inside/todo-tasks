65249ffbfebbe186d55327b6d0b1496aa1f3d810
Crypto-Hamster <arkadiusz.budkowski.dev@gmail.com>
2021-06-14 17:21:57 +0200

    ehh

--- src/main/java/com/HamsterInside/controller/TaskController.java
+++ src/main/java/com/HamsterInside/controller/TaskController.java
@@ -1,2 +1,68 @@
-package com.HamsterInside.controller;class TaskController {
+package com.HamsterInside.controller;
+
+import com.HamsterInside.model.Task;
+import com.HamsterInside.model.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+
+@RestController
+class TaskController
+{
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskRepository repository;
+
+    TaskController(final TaskRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks()
+    {
+        logger.warn("Exposing all the tasks");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page)
+    {
+        logger.warn("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate)
+    {
+        if (!repository.existsById(id))
+        {
+            return ResponseEntity.notFound().build();
+        }
+        toUpdate.setId(id);
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/tasks/{id}")
+    ResponseEntity<Task> getTask(@PathVariable int id)
+    {
+        logger.info("reading specific task");
+
+        return repository.findById(id)
+                .map(task -> ResponseEntity.ok(task))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/tasks")
+    ResponseEntity<Task> postTask(@RequestBody @Valid Task createTask){
+        Task result = repository.save(createTask);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
+    }
 }
